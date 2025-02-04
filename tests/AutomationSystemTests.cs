2f0378e106f8540e205c28bbcfef8bcafed4991b
Phill <philldamaskin@gmail.com>
2025-02-03 20:54:06 -0500

    initial commit

---
+++ tests/AutomationSystemTests.cs
@@ -0,0 +1,31 @@
+﻿using Simulation.Tests;
+using Types;
+using Worlds;
+
+namespace Automations.Systems.Tests
+{
+    public abstract class AutomationSystemTests : SimulationTests
+    {
+        static AutomationSystemTests()
+        {
+            TypeRegistry.Load<AutomationsTypeBank>();
+            TypeRegistry.Load<AutomationsSystemsTestsTypeBank>();
+        }
+
+        protected override Schema CreateSchema()
+        {
+            Schema schema = base.CreateSchema();
+            schema.Load<AutomationsSchemaBank>();
+            schema.Load<AutomationsSystemsTestsSchemaBank>();
+            return schema;
+        }
+
+        protected override void SetUp()
+        {
+            base.SetUp();
+            simulator.AddSystem<StateMachineSystem>();
+            simulator.AddSystem<StateAutomationSystem>();
+            simulator.AddSystem<AutomationPlayingSystem>();
+        }
+    }
+}
---
+++ tests/Position.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+using Worlds;
+
+namespace Automations.Systems.Tests
+{
+    [Component]
+    public struct Position
+    {
+        public Vector3 value;
+    }
+}
